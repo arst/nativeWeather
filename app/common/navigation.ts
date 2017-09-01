285a8d2b43ed10abfbadf05974de2a2da1c5477c
arst <artem.startsev@gmail.com>
2017-09-01 09:19:46 +0300

    Navigation module refactoring: const instead of redundant method call.  Removes comments.

--- app/common/navigation.ts
+++ app/common/navigation.ts
@@ -1,15 +1,11 @@
 import frame = require('ui/frame');
 
-//constant instead of this method?
-export function getStartPage() {
-    return 'pages/main/main';
-}
+export const START_PAGE = 'pages/main/main';
 
 export function goToForecastPage() {
     frame.topmost().navigate('pages/forecast/forecast');
 }
 
 export function goToMainPage() {
-    //this shall be refasctored to look like forecast so code lloks mnore consistent
     frame.topmost().navigate('pages/main/main');
 }
