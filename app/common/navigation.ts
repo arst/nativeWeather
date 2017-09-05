ae3558d5f3ee3183374eb0727dfd09e391f3e062
arst <artem.startsev@gmail.com>
2017-09-05 16:15:44 +0300

    UV index page navigation

--- app/common/navigation.ts
+++ app/common/navigation.ts
@@ -6,6 +6,10 @@ export function goToForecastPage() {
     frame.topmost().navigate('pages/forecast/forecast');
 }
 
+export function goToUVPage() {
+    frame.topmost().navigate("pages/uvindex/uvindex");
+}
+
 export function goToMainPage() {
     frame.topmost().navigate('pages/main/main');
 }
