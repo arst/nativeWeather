016fea39eca736715b2a8b47ea07685f9337e3f2
arst <artem.startsev@gmail.com>
2017-09-10 16:20:06 +0300

    api paths corrected

--- app/common/api_constants.ts
+++ app/common/api_constants.ts
@@ -1,4 +1,5 @@
 export const WEATHER_URL = 'http://api.openweathermap.org/data/2.5/';
-export const WEATHER_APIKEY = 'YOUR OPENWEATHERMAP API KEY';
+export const WEATHER_APIKEY = '';
 export const CURRENT_WEATHER_PATH = 'weather/';
-export const WEATHER_FORECAST_PATH = 'forecast/daily/';
+export const WEATHER_FORECAST_PATH = 'forecast';
+export const UVINDEX_PATH = 'uvi'
